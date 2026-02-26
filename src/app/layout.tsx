4205fecf683db037fd03ad7331a0cc56a39cbdce
deki84 <deki1984@hotmail.de>
2026-02-26 12:51:36 +0100

    feat: add Clerk auth, Neon DB setup and env config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,13 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import {
+  ClerkProvider ,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 
 export const metadata: Metadata = {
   title: "CryptoCalc",
@@ -10,10 +18,12 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className="min-h-screen bg-zinc-950 text-white">
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
